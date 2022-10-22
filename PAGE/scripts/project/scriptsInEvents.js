5cddcddcc1edc959401ce0b28a5d5ed52f1515d7
pinqtape <96379588+pinqtape@users.noreply.github.com>
2022-10-22 19:59:20 +0800

    aw
    daw

---
+++ PAGE/scripts/project/scriptsInEvents.js
@@ -0,0 +1,37 @@
+
+
+
+const scriptsInEvents = {
+
+	async Game_layout_Event5_Act1(runtime, localVars)
+	{
+		globalThis.addEventListener("keydown", e => {
+			if (e.code === "Tab")
+			{
+				e.preventDefault();
+			}
+		});
+	},
+
+	async Menu_layout_Event26(runtime, localVars)
+	{
+		
+		function keypress(event)
+		{
+			const list = "1234567890";
+			return (list.indexOf(String.fromCharCode(event.keyCode)) != -1);
+		}
+		
+		const objects = runtime.objects.TextInput.getAllInstances();
+		if (objects.length > 1)
+		{
+			for (let i = 0; i < objects.length; i++) CheckNumberBox[i].onkeypress = event => keypress(event);
+			return;
+		}
+		CheckNumberBox.onkeypress = event => keypress(event);
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
---
+++ add_info.php
@@ -0,0 +1,72 @@
+<?php require_once "setup.php";
+
+switch($json->type) {
+        case "add_kill_dead":
+            // who kill
+            add_info($json->who_kill, "kills", 1);
+            add_info($json->who_kill, "money", $json->mnojitel);
+            add_exp($json->who_kill, $json->mnojitel);
+
+            // who dead
+            add_info($json->who_dead, "deaths", 1);
+        break;
+
+        case "add_dead":
+            add_info($json->who_dead, "deaths", 1);
+            sub_info($json->who_dead, "money", 3);
+        break;
+
+        case "win_tour":
+            $obj = $json->team_win;
+            foreach ($obj as $user => $value) { // key = id, value = bet
+                add_info($user, "tours", 1);
+                foreach ($value as $key => $value) { // key = money  or cash, value = value
+                    add_info($user, $key, $value * 1.8);
+                };
+            };
+        break;
+
+        case "set_json":
+            $user = R::load('users', 42);
+            $data = json_decode($user->weapon, true);
+            $data["GUCCI"] = "5000$";
+            $user->weapon = json_encode($data);
+            R::store(($user));
+            echo json_encode($data);
+        break;
+
+        case "add_exp":
+           add_exp($json->id, $json->exp);
+        break;
+
+}             
+function add_info($id, $to, $value) {
+    $user = R::load('users', $id);
+    $user->$to += floor($value);
+    R::store(($user));
+};
+
+function sub_info ($id, $to, $value) {
+    $user = R::load('users', $id);
+    $user->$to -= $value;
+
+    if ($user->$to < 0) $user->$to = 0;
+    R::store(($user));
+}
+
+function add_exp($id, $value) {
+    $user = R::load('users', $id);
+    $check_lvl = R::load('lvl', $user->lvl);
+
+    $user->exp += $value;
+    if ($user->exp >= $check_lvl->exp) {
+        $user->lvl += 1;
+        $user->exp -= $check_lvl->exp;
+        $user->money += 500;
+    };
+    R::store(($user));
+};
+
+
+
+?>
