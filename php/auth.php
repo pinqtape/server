2ac55757421ce6b94ab6516a8ff1b7edbefd268c
pinqtape <96379588+pinqtape@users.noreply.github.com>
2022-10-25 04:31:07 +0800

    dwa
    dwa

---
+++ php/auth.php
@@ -0,0 +1,54 @@
+<?php require_once "setup.php";
+
+$user = R::findOne("users", "email = ?", array($json->email)); // ищу польователя по имейлу
+
+if ($user) { // если пользователь найден
+    if (password_verify($json->password, $user->password)) { // если пороль совпадает с хешем в таблице
+        $_SESSION["user"] = $user;
+
+        // $ip = "83.149.21.77";
+        // $user->address = ip2long($ip);
+        // R::store($user);
+
+        $current_ip = $_SERVER['REMOTE_ADDR'];;
+        $user_ip = long2ip($user->address);
+        echo $current_ip , $user_ip;
+        // if ($current_ip == $user_ip) {
+        //     echo json_encode([
+        //         "type" => "complete",
+        //         "action" => [
+        //             "id" => $user->id,
+        //             "clan" => $user->clan,
+        //             "nick" => $user->nickname,
+        //             "lvl" => $user->lvl,
+        //             "exp" => $user->exp,
+        //             "money" => $user->money,
+        //             "cash" => $user->cash
+        //         ]
+        //     ]);
+        // }
+        // else {
+        //     echo json_encode([
+        //         "type" => "ERROR",
+        //         "action" => "Your IP address is incorrect"
+        //     ]);
+        // };
+        
+    }
+    else {
+        echo json_encode([
+            "type" => "ERROR",
+            "action" => "invalid password"
+        ]);
+    }
+
+}
+
+else {
+    echo json_encode([
+        "type" => "ERROR",
+        "action" => "invalid email"
+    ]);
+}
+
+?>
---
+++ php/get_profile.php
@@ -0,0 +1,52 @@
+<?php require_once "setup.php";
+
+$user = R::findOne("users", "id = ?", array($json->id));
+switch($json->type) {
+    case "get_profile":
+        if ($user) {
+            echo json_encode([
+                "type" => "get_profile",
+                "action" => [
+                    "id" => $user->id,
+                    "nick" => $user->nickname,
+                    "lvl" => $user->lvl,
+                    "exp" => $user->exp,
+                    "money" => $user->money,
+                    "cash" => $user->cash,
+                    "kills" => $user->kills,
+                    "deaths" => $user->deaths,
+                    "weapon" => $user->weapon
+            
+                ]
+            ]);
+        } else {
+            echo json_encode([
+                "type" => "ERROR",
+                "action" => "user not found"
+            ]);
+        }
+    break;
+
+    case "info":
+        if ($user) {
+            echo json_encode([
+                "type" => "info",
+                "action" => [
+                    "id" => $user->id,
+                    "lvl" => $user->lvl,
+                    "exp" => $user->exp,
+                    "money" => $user->money,
+                    "cash" => $user->cash
+                ]
+            ]);
+        }
+    break;
+}
+
+
+
+
+
+
+
+?>
